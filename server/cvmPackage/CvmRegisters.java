b73805da6a3006a0aa6f8e29f044f7c2a1be4276
Abrabera <anastasya200608@gmail.com>
2025-11-27 15:13:55 +0100

    add unworking code

---
+++ server/cvmPackage/CvmRegisters.java
@@ -0,0 +1,64 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package cvmPackage;
+
+public class CvmRegisters {
+    static int cpuError = 0;
+    static boolean running = false;
+    static int REG = 0;
+    static int MEM = 0;
+    static Switch regSwitch;
+    static int PC;
+    static int SP;
+    static int A;
+    static int X;
+    static float R;
+    static int RH;
+    static int RL;
+    static int[] M;
+
+    public CvmRegisters() {
+    }
+
+    void CvmRegisters() {
+        for(int k = 0; k < 65536; ++k) {
+            M[k] = 0;
+        }
+
+    }
+
+    static {
+        regSwitch = CvmRegisters.Switch.selPC;
+        PC = 0;
+        SP = 0;
+        A = 0;
+        X = 0;
+        R = 0.0F;
+        RH = 0;
+        RL = 0;
+        M = new int[65536];
+    }
+
+    static enum RegMem {
+        selREG,
+        selMEM;
+
+        private RegMem() {
+        }
+    }
+
+    static enum Switch {
+        selPC,
+        selSP,
+        selA,
+        selX,
+        selRH,
+        selRL;
+
+        private Switch() {
+        }
+    }
+}
