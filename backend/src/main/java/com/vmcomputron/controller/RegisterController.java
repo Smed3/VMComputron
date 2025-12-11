3369d4a1db63fb81145e29214f558ec5dc800348
Abrabera <anastasya200608@gmail.com>
2025-12-11 19:49:03 +0100

    add push get register in socket

--- backend/src/main/java/com/vmcomputron/controller/RegisterController.java
+++ backend/src/main/java/com/vmcomputron/controller/RegisterController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api") //Путь для http запросов
-public class CompController {
+public class RegisterController {
     //эндпоинты
     @GetMapping("/{type}/{address}")
     public ResponseEntity<?> getRegister(@PathVariable String type,
--- backend/src/main/java/com/vmcomputron/controller/WebSocketController.java
+++ backend/src/main/java/com/vmcomputron/controller/WebSocketController.java
@@ -1,7 +1,10 @@
 package com.vmcomputron.controller;
 
+import com.vmcomputron.cvmPackage.CvmRegisters;
 import com.vmcomputron.model.Greeting;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
@@ -14,5 +17,36 @@ public class WebSocketController { // websocket
     public Greeting handle(String message) {
         return new Greeting("Привет от сервера: " + message);
     }
+
+    @MessageMapping("/registerUpdated")
+    @SendTo("/topic/registers")
+    public CvmRegisters handleRegisterUpdate(
+            @Payload Integer value,
+            @Header("register") String registerName) {
+
+        CvmRegisters.updateRegister(registerName, value);
+        return CvmRegisters.getCurrentState();
+    }
+//    client.publish({
+//        destination: '/app/registerUpdated',
+//                body: JSON.stringify(42),                    // ← значение
+//                headers: { 'register': 'A' }                 // ← имя регистра
+//    });
+//    client.subscribe('/topic/registers', (msg) => {
+//    const regs = JSON.parse(msg.body);
+//        setRegisters({
+//                pc: regs.pc.toString(16).padStart(4, '0').toUpperCase(),
+//                sp: regs.sp,
+//                a: regs.a,
+//                x: regs.x,
+//                r: regs.r.toFixed(2),
+//                rh: regs.rh,
+//                rl: regs.rl
+//    });
+//    });
+
+
+
+
 }
 
